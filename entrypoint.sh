e15440d3fb7e2afe24dbf75a66680aa5811a25cc
Viv Lim <git@vvn.space>
2024-05-19 01:22:19 -0700

    need to get the owner/group myself

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,4 +2,8 @@
 
 echo "Building site in working directory"
 cd /github/workspace
-/bin/zolra build && chown --reference=content public
+
+/bin/zolra build
+
+# need to fix permissions after building, or it'll be owned by root
+chown $(stat -c %u:%g .) -R public -c
